cca57f04bf17c6a02e209d42c1853e4028b107e2
malberda <malberda15@gmail.com>
2022-11-07 14:18:32 -0700

    started on basic stuff, going to upload design.pdf later, dont have access right now

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<head>
+    <title>Final Project CS 401</title>
+    <link rel="stylesheet" href="ref/styles.css">
+</head>
+    <div id="title" class='pageTitle'>
+        This is my Final Project for CS 401
+    </div>
+
+    <div class='flex-container'>
+
+        <div class="flex-child-left">
+            Flex Column 1
+        </div>
+  
+        <div class="flex-child-right">
+            Flex Column 2
+        </div>
+    </div>
+    <a href='/uploadBlog.php'>
+        <button class="newBlogButton">Upload New Blog</button>
+    </a>
+<?php 
+
+
+
+?>
