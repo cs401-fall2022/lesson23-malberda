d3a5913ae2cfdac6d1f62d3104c9108139aa2019
malberda <malberda15@gmail.com>
2022-11-13 09:31:53 -0700

    basic db stuff working

--- uploadBlog.php
+++ uploadBlog.php
@@ -5,16 +5,16 @@
 <body>
     <form method="post" action="upload_handler.php">
         <div>
-           <label for blog_title>Input Blog Title</label><br>
-            <input id="blog_title"><br><br><br>
+           <label for="blog_title">Input Blog Title</label><br>
+            <input id="blog_title" name="blog_title" required><br><br><br>
         </div>
         <div>
-            <label for blog_contents>Input Blog Contents:</label><br>
-            <textarea id="blog_contents" rows='45' cols='250'></textarea><br><br>
+            <label for="blog_contents">Input Blog Contents:</label><br>
+            <textarea id="blog_contents" name="blog_contents" rows='45' cols='250' required></textarea><br><br>
         </div>
         <div>
             <label for submit>Submit New Blog</label> 
-            <input type="submit">
+            <input type="submit" id='submit' name='submit'>
         </div>
     </form>
 </body>
--- upload_handler.php
+++ upload_handler.php
@@ -2,22 +2,40 @@
 <?php
     echo "please help";
 
+    // Create connection
+    $conn = new mysqli("h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "wssr4ahq7pc7kym9", "b1yaul4q9uq2pymr", "t8j4x8c83vv4nopt");
 
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $query = "SELECT * FROM blogs;";
+    $results = $conn->query($query);
+echo "this test is working 1";
     try {
-        $title = $POST['blog_title'];
-        $contents = $POST['blog_contents'];
-        echo $title. ' '.$contents . 'those were the title and contents';
-        $connection = $this->getConnection();
-        $q = $connection->prepare("INSERT INTO blogs (blog_title, blog)
-                                    VALUES(:title, :contents);");
-        $q->bindParam(":title", $title);
-        $q->bindParam(":contents", $contents);
-        $q->execute();
+        echo "this test is working 2";
+        echo print_r($_POST);
+        echo "this test is working 3";
+        $title = $_POST['blog_title'];
+        echo "this test is working 4";
+echo $title . '<br>';
+echo "this test is working 5";
+        $contents = $_POST['blog_contents'];
+        echo "this test is working 6";
+        echo $contents . '<br>';
+
+        $sql = "INSERT INTO blogs (blog_title, blog) VALUES ('$title','$contents');";
+        if($conn->query($sql) == true) {
+            echo "new record created successfully";
+        }
+        else {
+            echo "error" . $sql."<br>" . $conn->error;
+        }
     } catch(Exception $e) {
         echo print_r($e,1);
         //exit;
     }
-    echo print_r($q);
+    echo print_r($sql);
     //exit;
 
 ?>
